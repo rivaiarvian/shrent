ff24e037aaa364f9713339c9d96669360c7f61f3
rivaiarvian <rivai.arviannn@gmail.com>
2024-08-31 02:12:11 +0700

    development model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'category_id',
+        'brand_id',
+        'price',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'brand_id');
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(ProductPhoto::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
